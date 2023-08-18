28b7cba055b4c5db6a423a9b43fd1bc2e8ea3c3c
Ryder Rhoads <ryder.rhoads10@gmail.com>
2023-08-18 01:14:19 -0700

    created repo

---
+++ __main__.py
@@ -0,0 +1,43 @@
+def text_to_binary(text):
+    binary_text = ' '.join(format(ord(char), '08b') for char in text)
+    return binary_text
+
+def binary_to_text(binary_text):
+    binary_words = binary_text.split()
+    text = ''.join(chr(int(binary, 2)) for binary in binary_words)
+    return text
+
+def main():
+    choice = input("Enter 'encode' to encode text to binary or 'decode' to decode binary to text: ")
+
+    if choice.lower() == 'encode':
+        input_file_path = input("Enter the path to the input text file: ")
+        output_file_path = input("Enter the path for the output binary file: ")
+
+        with open(input_file_path, 'r') as file:
+            content = file.read()
+
+        binary_content = text_to_binary(content)
+
+        with open(output_file_path, 'w') as file:
+            file.write(binary_content)
+        print("Text encoded to binary and saved to", output_file_path)
+
+    elif choice.lower() == 'decode':
+        input_file_path = input("Enter the path to the input binary file: ")
+        output_file_path = input("Enter the path for the output text file: ")
+
+        with open(input_file_path, 'r') as file:
+            binary_content = file.read()
+
+        text_content = binary_to_text(binary_content)
+
+        with open(output_file_path, 'w') as file:
+            file.write(text_content)
+        print("Binary decoded to text and saved to", output_file_path)
+
+    else:
+        print("Invalid choice. Please enter 'encode' or 'decode'.")
+
+if __name__ == "__main__":
+    main()
